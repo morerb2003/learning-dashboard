fc5f3212513c7c2cb7d466e70f1be3817c5de270
morerb2003 <119853713+morerb2003@users.noreply.github.com>
2026-09-16 14:45:27 +0530

    fix(motion): add explicit 'use client' directive to motion barrel export

--- components/motion/index.ts
+++ components/motion/index.ts
@@ -1,6 +1,9 @@
+"use client";
+
 export { default as TiltCard } from "./TiltCard";
 export { default as SpotlightCard } from "./SpotlightCard";
 export { default as AnimatedCounter } from "./AnimatedCounter";
 export { FadeIn, Stagger, StaggerItem } from "./FadeIn";
 export { default as TextReveal } from "./TextReveal";
 export { default as FloatingBadge } from "./FloatingBadge";
+
